f8d438582494da90953402bbc6c65156f7a299c3
Шерстобоев Илья <sherstoboevilya@gmail.com>
2025-02-10 13:25:01 +0500

    Добавил работу с DOM в компоненте TheCounter Вызываю клик и жду пока отрендерится новый DOM перед выполнением теста

--- src/components/__tests__/TheCounter.spec.ts
+++ src/components/__tests__/TheCounter.spec.ts
@@ -1,18 +1,29 @@
 import { mount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
 import TheCounter from '../TheCounter.vue'
+import { nextTick } from 'vue'
 
 describe('TheCounter', () => {
-  const wrapper = mount(TheCounter)
+  const initWrapper = () => {
+    const wrapper = mount(TheCounter)
+    const button = wrapper.find('button')
+
+    return {
+      wrapper,
+      button,
+    }
+  }
 
   it('emits', () => {
-    wrapper.trigger('click')
+    const { wrapper, button } = initWrapper()
+
+    button.trigger('click')
     const increment = wrapper.emitted('increment')
 
     expect(increment).toHaveLength(1)
 
-    wrapper.trigger('click')
-    wrapper.trigger('click')
+    button.trigger('click')
+    button.trigger('click')
 
     expect(increment).toHaveLength(3)
     expect(increment?.[0]).toEqual([
@@ -27,5 +38,14 @@ describe('TheCounter', () => {
         isEven: true,
       },
     ])
+  })
+
+  it('async render DOM', async () => {
+    const { wrapper, button } = initWrapper()
+
+    button.trigger('click')
+    await nextTick()
+
+    expect(wrapper.find('label').text()).toContain('Count: 1')
   })
 })
